21ff04f66dd6abcdc5954b6b5a9c489e60709cb9
lquezada <que18028@uvg.edu.gt>
2020-02-14 02:12:51 -0600

    DarkPDF V1

--- DarkPDF.py
+++ DarkPDF.py
@@ -22,19 +22,28 @@ def toDark(image, threshold):
 	for i in range(0,len(arr)):
 		for j in range(0,len(arr[i])):
 			if arr[i][j] >= threshold:
-				arr[i][j] = 30
+				arr[i][j] = 20
 			else:
 				arr[i][j] = 255
 	return PIL.Image.fromarray(arr)
 
+# Intro banner
+print("\n\n")
+print("  _____             _    _____  _____  ______ ")
+print(" |  __ \           | |  |  __ \|  __ \|  ____|")
+print(" | |  | | __ _ _ __| | _| |__) | |  | | |__   ")
+print(" | |  | |/ _` | '__| |/ /  ___/| |  | |  __|  ")
+print(" | |__| | (_| | |  |   <| |    | |__| | |     ")
+print(" |_____/ \__,_|_|  |_|\_\_|    |_____/|_|     ")
+
 # Convert to dark mode and save pdf
-print("\n+ Retrieving " + fileName)
+print("\n> Retrieving " + fileName)
 images = convert_from_path(fileName)
 darkImages = []
 
 
 for i in range(0,len(images)):
-	print("+ Converting page " + str(i+1))
+	print(">> Converting page " + str(i+1))
 	darkImages.append(toDark(images[i],125))
 
 
@@ -42,7 +51,7 @@ for i in range(0,len(images)):
 fileName = fileName.replace('.pdf','')
 newFileName = fileName + "-dark.pdf"
 
-print("+ Saving " + newFileName)
+print("> Saving " + newFileName + "\n")
 darkImages[0].save(newFileName,format='pdf',save_all=True,append_images=darkImages[1:])
 darkImages[1].save('new.pdf')
 
